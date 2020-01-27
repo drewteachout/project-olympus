261faeadd6dfeb54c1434ed396b1003ef8c125b5
drewteachout <Zflicka1>
2020-01-27 17:40:32 -0500

    Added angular material. Created main menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { MatButtonModule } from '@angular/material/button';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { MainMenuComponent } from './main-menu/main-menu.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainMenuComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatGridListModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-menu/main-menu.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
+
+@Component({
+  selector: 'app-main-menu',
+  templateUrl: './main-menu.component.html',
+  styleUrls: ['./main-menu.component.scss']
+})
+export class MainMenuComponent implements OnInit {
+
+  tiles: Tile[] = [
+    {text: 'My Games', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: '', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: '', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: '', cols: 1, rows: 1, color: 'lightgreen'},
+    {text: '', cols: 2, rows: 1, color: 'lightgreen'},
+  ]
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
